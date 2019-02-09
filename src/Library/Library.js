e21918c17fba310581b924b05bce085af0d9b8f7
Lawrence Ingraham <lawrence@Lawrences-iMac.local>
2019-02-09 17:46:26 -0500

    Styles tweaking

--- src/Library/Library.js
+++ src/Library/Library.js
@@ -38,7 +38,7 @@ class Library extends Component {
           <span>12 Books</span>
           <button className='library-add'>
             <i>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#004BAF" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"/></svg>                  
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#004BAF" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"/></svg>                  
             </i>
             <span>Add Book</span>
           </button>
